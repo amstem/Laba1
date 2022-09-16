7c71304dc44f51a4d770ca988146640a916caac6
amstem <81528560+amstem@users.noreply.github.com>
2022-09-16 20:27:54 +0400

    Add files via upload

---
+++ laba1_7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace laba1_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a,b;
+            Console.WriteLine("Введите значение a: ");
+            while (!byte.TryParse(Console.ReadLine(), out a) || a < 0)
+            {
+                Console.WriteLine("Не верный ввод данных");
+            }
+            Console.WriteLine("Введите значение b: ");
+            while (!byte.TryParse(Console.ReadLine(), out b) || b < 0)
+            {
+                Console.WriteLine("Не верный ввод данных");
+            }
+            Console.WriteLine("«побитовое И» == " + (a & b));
+            Console.WriteLine("«побитовое ИЛИ» == " + (a | b));
+            Console.WriteLine("«побитовое исключающее ИЛИ» == " + (a ^ b));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ lava1_4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 0;
+            Console.WriteLine("Введите значение: ");
+            while (!int.TryParse(Console.ReadLine(), out m))
+            {
+                Console.WriteLine("Можно вводить только целые числа");
+            }
+            if (m < 0)
+            {
+                Console.WriteLine("Нельзя вводить значение, меньше нуля");
+            }
+
+            for (int x = 0; x < m; x++)
+            {
+                int e = 1;
+                for (int n=0;n<=x;n++)
+                {
+                    for(int c=0;c<m-n;c++)
+                    {
+                        Console.Write(' ');
+                    }
+                    for (int c = 0; c < e; c++)
+                    {
+                        Console.Write('*');
+                       
+                    }
+                    e+=2; Console.WriteLine();
+                    
+                }
+
+            }   
+
+            Console.ReadLine();
+        }
+    }
+}
