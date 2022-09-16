557f21b780e2aa7fde7e9f3c3b3360d118128ecc
amstem <81528560+amstem@users.noreply.github.com>
2022-09-16 20:24:26 +0400

    Add files via upload

---
+++ laba 1_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Введите значение" );
+            int m;
+            while (!int.TryParse(Console.ReadLine(), out m))
+            {
+                Console.WriteLine("Можно вводить только целые числа");
+            }
+            for (int j=0;j<m;j++)
+            {
+                
+                for (int o = 0; o < j; o++)
+                {
+
+                    Console.Write("*");
+                }
+                Console.WriteLine("*");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ laba1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace laba1
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Введите сторону a: ");
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Можно вводить только целые числа");
+            }
+
+            int b;
+            Console.WriteLine("Введите сторону b:  ");
+            while (!int.TryParse(Console.ReadLine(), out b))
+            {
+                Console.WriteLine("Можно вводить только целые числа");
+            }
+            int qw = -1;
+            int m = a * b;
+            if (m < 0)
+            {
+                int c = m * qw;
+                Console.WriteLine(c);
+            }
+            else
+            Console.WriteLine(m);
+            Console.ReadKey();
+
+        }
+    }
+}
